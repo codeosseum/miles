ed01207c98bfa493f6122e2ac2d9d1caf10079c0
Attila Bagossy <bagossyattila@outlook.com>
2019-01-11 00:06:15 +0100

    Minor fixes in Message and HttpPushMessageToAresServiceImpl

--- src/main/java/com/codeosseum/miles/communication/Message.java
+++ src/main/java/com/codeosseum/miles/communication/Message.java
@@ -2,16 +2,16 @@ package com.codeosseum.miles.communication;
 
 import java.util.Objects;
 
-public final class Message<T> {
+public class Message<T> {
     private final String action;
 
     private final T payload;
 
-    private static <T> Message<T> message(final String action, final T payload) {
+    public static <T> Message<T> message(final String action, final T payload) {
         return new Message<>(Objects.requireNonNull(action), Objects.requireNonNull(payload));
     }
 
-    private Message(final String action, final T payload) {
+    protected Message(final String action, final T payload) {
         this.action = action;
         this.payload = payload;
     }
--- src/main/java/com/codeosseum/miles/communication/http/push/HttpPushMessageToAresServiceImpl.java
+++ src/main/java/com/codeosseum/miles/communication/http/push/HttpPushMessageToAresServiceImpl.java
@@ -8,12 +8,19 @@ import com.mashape.unirest.http.Unirest;
 import java.util.Objects;
 
 public class HttpPushMessageToAresServiceImpl implements PushMessageToAresService {
-    private static final String ARES_HOST = "127.0.0.1:8080";
+    private static final String EVENT_IDENTIFIER_HEADER = "X-Event-Identifier";
+
+    private static final String ARES_HOST = "http://127.0.0.1:8080";
 
     @Override
     public <T> void sendMessage(final Message<T> message) {
+        Objects.requireNonNull(message);
+
         try {
-            Unirest.post(ARES_HOST).body(Objects.requireNonNull(message)).asString();
+            Unirest.post(ARES_HOST)
+                .header(EVENT_IDENTIFIER_HEADER, message.getAction())
+                .body(message.getPayload())
+                .asString();
         } catch (Exception e) {
             throw new PushMessageException(e);
         }
