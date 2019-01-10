dd038581892100fec869d024ec28a4fef2ab736b
Attila Bagossy <bagossyattila@outlook.com>
2019-01-10 16:19:58 +0100

    Added WebSocket session handling capabilities

---
+++ src/main/java/com/codeosseum/miles/messaging/websocket/session/SessionRegistry.java
@@ -0,0 +1,26 @@
+package com.codeosseum.miles.messaging.websocket.session;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+public interface SessionRegistry {
+    Set<Session> getActiveSessions();
+
+    void addActiveSession(Session session);
+
+    void removeActiveSession(Session session);
+
+    void addAuthenticatedSession(Session session, long id);
+
+    void removeAuthenticatedSession(Session session);
+
+    Map<Long, Session> getAuthenticatedSessions();
+
+    Optional<Long> getIdForSession(Session session);
+
+    Optional<Session> getSessionForId(long id);
+}
+
