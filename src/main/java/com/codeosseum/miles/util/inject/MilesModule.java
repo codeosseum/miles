ff7b1f53046578dbbaaf645716f39ca5931af0c8
Attila Bagossy <bagossyattila@outlook.com>
2019-01-28 23:32:50 +0100

    Singleton helpers in MilesModule

--- src/main/java/com/codeosseum/miles/util/inject/MilesModule.java
+++ src/main/java/com/codeosseum/miles/util/inject/MilesModule.java
@@ -5,10 +5,13 @@ import com.codeosseum.miles.util.inject.attach.WebSocketControllerAttacher;
 import com.codeosseum.miles.util.inject.initialization.PostConstructListener;
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
+import com.google.inject.Singleton;
 import com.google.inject.matcher.Matchers;
 
 import java.util.Collections;
 import java.util.List;
+
+import static java.util.Objects.requireNonNull;
 
 public class MilesModule extends AbstractModule {
     protected void configureModule() {
@@ -35,4 +38,22 @@ public class MilesModule extends AbstractModule {
     }
 
     protected List<Module> installs() { return Collections.emptyList(); }
+
+    protected final <T> void bindSingleton(final Class<T> clazz) {
+        bind(requireNonNull(clazz)).in(Singleton.class);
+    }
+
+    protected final <T, S extends T> void bindSingleton(final Class<T> clazz, final Class<S> subclazz) {
+        bind(requireNonNull(subclazz)).in(Singleton.class);
+        bind(requireNonNull(clazz)).to(subclazz);
+    }
+
+    protected final <T> void bindEagerSingleton(final Class<T> clazz) {
+        bind(requireNonNull(clazz)).asEagerSingleton();
+    }
+
+    protected final <T, S extends T> void bindEagerSingleton(final Class<T> clazz, final Class<S> subclazz) {
+        bind(requireNonNull(subclazz)).asEagerSingleton();
+        bind(requireNonNull(clazz)).to(subclazz);
+    }
 }
