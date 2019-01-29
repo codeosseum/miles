903c592891a4f2789b1260bfb9d14b432ccc801a
Attila Bagossy <bagossyattila@outlook.com>
2019-01-29 18:59:32 +0100

    Created the BindingRemoverListener

---
+++ src/main/java/com/codeosseum/miles/code/execution/listeners/BindingRemoverListener.java
@@ -0,0 +1,27 @@
+package com.codeosseum.miles.code.execution.listeners;
+
+import java.util.List;
+
+import com.codeosseum.miles.code.execution.CodeExecutionListener;
+import org.graalvm.polyglot.Context;
+
+import static java.util.Objects.requireNonNull;
+
+public class BindingRemoverListener extends CodeExecutionListener {
+    private static final String JAVASCRIPT = "js";
+
+    private final List<String> bindings;
+
+    public static BindingRemoverListener fromBindings(List<String> bindings) {
+        return new BindingRemoverListener(requireNonNull(bindings));
+    }
+
+    private BindingRemoverListener(List<String> bindings) {
+        this.bindings = bindings;
+    }
+
+    @Override
+    public void onContextBuilt(final Context context) {
+        bindings.forEach(name -> context.getBindings(JAVASCRIPT).removeMember(name));
+    }
+}
