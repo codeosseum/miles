373461e3181565a494e889a3857cfb39a73cc0bb
Attila Bagossy <bagossyattila@outlook.com>
2019-02-03 14:03:48 +0100

    Extracted constants in ModuleExportsListener

--- src/main/java/com/codeosseum/miles/code/execution/listener/listeners/ModuleExportsListener.java
+++ src/main/java/com/codeosseum/miles/code/execution/listener/listeners/ModuleExportsListener.java
@@ -10,25 +10,31 @@ import static java.util.Objects.isNull;
 public class ModuleExportsListener extends CodeExecutionListener {
     private static final String JAVASCRIPT = "js";
 
+    private static final String MODULE = "module";
+
+    private static final String EXPORTS = "exports";
+
+    private static final String EMPTY_OBJECT_EXPRESSION = "({})";
+
     private Value moduleBinding;
 
     @Override
     public void onBeforeExecute(final ListenerCodeExecutor.PreExecutionContext preExecutionContext) {
-        moduleBinding = preExecutionContext.getContext().eval(Source.create(JAVASCRIPT, "({})"));
+        moduleBinding = preExecutionContext.getContext().eval(Source.create(JAVASCRIPT, EMPTY_OBJECT_EXPRESSION));
 
-        preExecutionContext.getContext().getBindings(JAVASCRIPT).putMember("module", moduleBinding);
+        preExecutionContext.getContext().getBindings(JAVASCRIPT).putMember(MODULE, moduleBinding);
     }
 
     @Override
     public void onAfterExecute(final ListenerCodeExecutor.PostExecutionContext postExecutionContext) {
-        postExecutionContext.getContext().getBindings(JAVASCRIPT).removeMember("module");
+        postExecutionContext.getContext().getBindings(JAVASCRIPT).removeMember(MODULE);
     }
 
     public Value get() {
         if (isNull(moduleBinding)) {
              return null;
         } else {
-            return moduleBinding.getMember("exports");
+            return moduleBinding.getMember(EXPORTS);
         }
     }
 }
