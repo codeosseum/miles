3071c03d2e8561199712fba81b1e9e66e6430e74
Attila Bagossy <bagossyattila@outlook.com>
2019-02-13 20:33:43 +0100

    Now able to send empty messages

--- src/main/java/com/codeosseum/miles/communication/Message.java
+++ src/main/java/com/codeosseum/miles/communication/Message.java
@@ -1,14 +1,18 @@
 package com.codeosseum.miles.communication;
 
-import java.util.Objects;
+import static java.util.Objects.requireNonNull;
 
 public class Message<T> {
     private final String action;
 
     private final T payload;
 
+    public static Message<Void> emptyMessage(final String action) {
+        return new Message<>(requireNonNull(action), null);
+    }
+
     public static <T> Message<T> message(final String action, final T payload) {
-        return new Message<>(Objects.requireNonNull(action), Objects.requireNonNull(payload));
+        return new Message<>(requireNonNull(action), requireNonNull(payload));
     }
 
     protected Message(final String action, final T payload) {
