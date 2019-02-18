c47c99e058066a0155a28c6c0a2c5d81b38665db
Attila Bagossy <bagossyattila@outlook.com>
2019-02-18 23:32:10 +0100

    Setting status to clean up when the match is over

--- src/main/java/com/codeosseum/miles/faultseeding/match/over/DefaultMatchTimerImpl.java
+++ src/main/java/com/codeosseum/miles/faultseeding/match/over/DefaultMatchTimerImpl.java
@@ -5,12 +5,16 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import com.codeosseum.miles.eventbus.dispatch.EventDispatcher;
+import com.codeosseum.miles.faultseeding.match.Constants;
+import com.codeosseum.miles.match.MatchStatus;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
 public class DefaultMatchTimerImpl implements MatchTimer {
     // TODO: Read from configuration.
     private static final long COUNTDOWN_MILLISECONDS = 30000;
+
+    private final MatchStatus matchStatus;
 
     private final EventDispatcher eventDispatcher;
 
@@ -19,7 +23,8 @@ public class DefaultMatchTimerImpl implements MatchTimer {
     private LocalDateTime startedAt;
 
     @Inject
-    public DefaultMatchTimerImpl(final EventDispatcher eventDispatcher, @Named("match-timer-timer") final Timer timer) {
+    public DefaultMatchTimerImpl(final MatchStatus matchStatus, final EventDispatcher eventDispatcher, @Named("match-timer-timer") final Timer timer) {
+        this.matchStatus = matchStatus;
         this.eventDispatcher = eventDispatcher;
         this.timer = timer;
     }
@@ -31,6 +36,8 @@ public class DefaultMatchTimerImpl implements MatchTimer {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
+                matchStatus.setCurrentStage(Constants.Stage.CLEANUP);
+
                 eventDispatcher.dispatchEvent(new MatchOverSignal());
             }
         }, COUNTDOWN_MILLISECONDS);
